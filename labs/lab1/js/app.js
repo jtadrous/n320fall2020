0ebb6ce459a5c570a2a3f8caca1cc56415bebe2f
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-08 16:54:21 -0400

    added 'createRect' function and instance of Rect class

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -1,14 +1,3 @@
-function draw() {
-
-}
-
-function setup() {
-    createCanvas(800,600);
-    background(170, 222, 240);
-    fill(51);
-    rect(0, 520, 800, 80);
-}
-
 class Rect {
     constructor(x, y, width, height) {
         this.x = x;
@@ -16,4 +5,20 @@ class Rect {
         this.width = width;
         this.height = height;
     }
+
+    createRect(){
+        rect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function setup() {
+    createCanvas(800,600);
+    //fill(51);
+}
+
+function draw() {
+    background(170, 222, 240);
+    fill(51);
+    var rect = new Rect(0, 520, 800, 80);
+    rect.createRect();
 }
