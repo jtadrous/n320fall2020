e6a4253ba54d76eabfe5ebc2b4265ab8cb3c9efd
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-08 22:36:51 -0400

    added color attribute to 'Rect' class

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -3,11 +3,12 @@
 //N320
 
 class Rect {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, color) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
+        this.color = color;
     }
 
     createRect() {
