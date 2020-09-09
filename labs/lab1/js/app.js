d4e3338454bdf02be19b1a66abdd609050f3404e
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-08 23:37:45 -0400

    moved if statement down into createDrop function

--- labs/lab1/js/app.js
+++ labs/lab1/js/app.js
@@ -59,6 +59,11 @@ class Raindrop {
     createDrop() {
         this.y += this.speed;
         ellipse(this.x, this.y, this.width, this.height, this.color);
+        if(this.drops.y >= 520) {
+            counter ++;
+            this.drops.y = 0;
+        }
+        return counter
     }
 }
 
