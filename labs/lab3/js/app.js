e9c93c2daeb1349e21fccfeca881a3721fd99705
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-22 17:16:35 -0400

    created 3 properties for the VirtualMachine class

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -1,5 +1,7 @@
 class VirtualMachine {
     constructor() {
-        
+        this.firstCandy = {name: "Kit-Kat", count: 50, taste:"sweet"}
+        this.secondCandy = {name: "Skittles", count: 50, taste: "fruity"}
+        this.firstChips = {name: "Ruffles", count: 50, taste: "savory"}
     }
 }
