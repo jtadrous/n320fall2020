9691bb57608afd074d2240545d42f3373c61b229
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-22 23:07:16 -0400

    commenting

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -1,18 +1,26 @@
+//new class for the overall vending machine
 class VirtualMachine {
     constructor() {
+        //three properties in this class: one for each snack (each snack holds an object)
         this.firstCandy = {name: "Kit-Kat", count: 50, taste:"Sweet"}
         this.secondCandy = {name: "Skittles", count: 50, taste: "Fruity"}
         this.firstChips = {name: "Ruffles", count: 50, taste: "Savory"}
     }
 
+    //this method recieved the name of the button that was pressed and changes the count number
     buySnack(candy) {
+        //this if statement checks to see if the button name equals name of the object and if the
+        //count number is greater than zero
         if(candy == "firstCandy" && this.firstCandy.count > 0) {
+            //then it subtracts one from the candy count number
             this.firstCandy.count--;
         } else if (candy == "secondCandy" && this.secondCandy.count > 0) {
             this.secondCandy.count--;
         } else if (candy == "firstChips" && this.firstChips.count > 0){
             this.firstChips.count--;
         }
+        //after running the if statement, it will resubmit the template to the HTML div by calling
+        //the render() method
         myDiv.innerHTML = this.render();
     }
 
