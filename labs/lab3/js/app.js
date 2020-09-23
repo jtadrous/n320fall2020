f587091f7c910472ddc06b40978f2c99e715b792
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-22 23:16:19 -0400

    commenting

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -1,15 +1,15 @@
-//new class for the overall vending machine
+//new class that represents the overall vending machine
 class VirtualMachine {
     constructor() {
-        //three properties in this class: one for each snack (each snack holds an object)
+        //three properties in this class: one for each snack (each snack holds an object with details)
         this.firstCandy = {name: "Kit-Kat", count: 50, taste:"Sweet"}
         this.secondCandy = {name: "Skittles", count: 50, taste: "Fruity"}
         this.firstChips = {name: "Ruffles", count: 50, taste: "Savory"}
     }
 
-    //this method recieved the name of the button that was pressed and changes the count number
+    //this method receives the name of the button that was pressed and changes the according count number
     buySnack(candy) {
-        //this if statement checks to see if the button name equals name of the object and if the
+        //this if statement checks to see if the button name equals the name of the object and if the
         //count number is greater than zero
         if(candy == "firstCandy" && this.firstCandy.count > 0) {
             //then it subtracts one from the candy count number
@@ -24,6 +24,8 @@ class VirtualMachine {
         myDiv.innerHTML = this.render();
     }
 
+    //created a new method called render() to return a JS Template string of HTML code displaying
+    //the information within the snack objects
     render() {
         return `
         <h2>${this.firstCandy.name}</h2>
@@ -45,6 +47,10 @@ class VirtualMachine {
     }
 }
 
+//this variable holds a new instance of the VirtualMachine() class, basically I created a new
+//vending machine.
 let myMach = new VirtualMachine();
+//this line grabs the HTML element with the id of "info" and stores it in a variable
 let myDiv = document.getElementById("info");
+//this line sets the innerHTML of that HTML element to the new template within the render() method
 myDiv.innerHTML = myMach.render();
