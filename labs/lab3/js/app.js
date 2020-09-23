5795c84a6661025fc97d11be0ecd05858cf712b9
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-22 22:57:37 -0400

    called the render method inside the buySnack method

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -9,33 +9,30 @@ class VirtualMachine {
         if(candy == "firstCandy") {
             this.firstCandy.count--;
         } else if (candy == "secondCandy") {
-            this.secondCandy.conut--;
+            this.secondCandy.count--;
         } else if (candy == "firstChips"){
             this.firstChips.count--;
         }
-        //this.render();
+        myDiv.innerHTML = this.render();
     }
 
     render() {
         return `
         <h2>${this.firstCandy.name}</h2>
         <ul>
-        <li>Taste: ${this.firstCandy.taste}</li>
+        <li>Type: ${this.firstCandy.taste}</li>
         <li>Available: ${this.firstCandy.count}</li>
         </ul>
-        <button id="firstCandy" onclick="${this.buySnack("firstCandy")}">Buy Candy</button>
         <h2>${this.secondCandy.name}</h2>
         <ul>
-        <li>Taste: ${this.secondCandy.taste}</li>
+        <li>Type: ${this.secondCandy.taste}</li>
         <li>Available: ${this.secondCandy.count}</li>
         </ul>
-        <button id="secondCandy" onclick="${this.buySnack("secondCandy")}">Buy Candy</button>
         <h2>${this.firstChips.name}</h2>
         <ul>
-        <li>Taste: ${this.firstChips.taste}</li>
+        <li>Type: ${this.firstChips.taste}</li>
         <li>Available: ${this.firstChips.count}</li>
         </ul>
-        <button id="firstChips" onclick="${this.buySnack("firstChips")}">Buy Chips</button>
         `;
     }
 }
@@ -43,4 +40,3 @@ class VirtualMachine {
 let myMach = new VirtualMachine();
 let myDiv = document.getElementById("info");
 myDiv.innerHTML = myMach.render();
-console.log(myDiv.innerHTML);
