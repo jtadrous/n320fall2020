0e5ae17ee7ed9bb5bbd592dd70948625d0e0081c
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-09-22 21:51:32 -0400

    editing return statement for template string

--- labs/lab3/js/app.js
+++ labs/lab3/js/app.js
@@ -1,8 +1,8 @@
 class VirtualMachine {
     constructor() {
-        this.firstCandy = {name: "Kit-Kat", count: 50, taste:"sweet"}
-        this.secondCandy = {name: "Skittles", count: 50, taste: "fruity"}
-        this.firstChips = {name: "Ruffles", count: 50, taste: "savory"}
+        this.firstCandy = {name: "Kit-Kat", count: 50, taste:"Sweet"}
+        this.secondCandy = {name: "Skittles", count: 50, taste: "Fruity"}
+        this.firstChips = {name: "Ruffles", count: 50, taste: "Savory"}
     }
 
     buySnack(candy) {
@@ -20,20 +20,20 @@ class VirtualMachine {
         return `
         <h2>${this.firstCandy.name}</h2>
         <ul>
-        <li>${this.firstCandy.taste}</li>
-        <li>${this.firstCandy.count}</li>
+        <li>Taste: ${this.firstCandy.taste}</li>
+        <li>Available: ${this.firstCandy.count}</li>
         </ul>
         <button id="firstCandy" onclick="${this.buySnack("firstCandy")}">Buy Candy</button>
         <h2>${this.secondCandy.name}</h2>
         <ul>
-        <li>${this.secondCandy.taste}</li>
-        <li>${this.secondCandy.count}</li>
+        <li>Taste: ${this.secondCandy.taste}</li>
+        <li>Available: ${this.secondCandy.count}</li>
         </ul>
         <button id="secondCandy" onclick="${this.buySnack("secondCandy")}">Buy Candy</button>
         <h2>${this.firstChips.name}</h2>
         <ul>
-        <li>${this.firstChips.taste}</li>
-        <li>${this.firstChips.count}</li>
+        <li>Taste: ${this.firstChips.taste}</li>
+        <li>Available: ${this.firstChips.count}</li>
         </ul>
         <button id="firstChips" onclick="${this.buySnack("firstChips")}">Buy Chips</button>
         `;
