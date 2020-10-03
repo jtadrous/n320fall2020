9a638f2e8d914c67d0228dbcbe218fffea246b38
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-02 23:05:57 -0400

    changed counter number

--- labs/lab4/js/app.js
+++ labs/lab4/js/app.js
@@ -6,22 +6,23 @@ function drawCanvas() {
 
 addEventListener("click", onPlayClick)
 function onPlayClick(event) {
-    console.log(event.target.getAttribute("xlink:href"));
+    //console.log(event.target.getAttribute("xlink:href"));
     if(event.target.getAttribute("data-name") != "Rectangle 2") {
         return;
-    } else if(counter > 9) {
+    } else if(counter > 8) {
         return;
     } else if(counter % 2 == 0) {
         event.target.setAttribute("xlink:href", "oPlayer.png"); 
         //console.log(event.target.getAttribute("xlink:href"));
-        console.log(event.target.id)
-        console.log(counter);
-        console.log(counter % 2);
+        //console.log(event.target.id)
+        //console.log(counter);
+        //console.log(counter % 2);
     } else {
         event.target.setAttribute("xlink:href", "xPlayer.png"); 
-        console.log(event.target.id);
-        console.log(counter);
-        console.log(counter % 2);
+        //console.log(event.target.id);
+        //console.log(counter);
+        //console.log(counter % 2);
     }
+    console.log(counter);
     counter++;
 }
