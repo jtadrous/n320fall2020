bedc1920dd16f7dc21663543cb8d3faa6cfb7e20
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-02 22:20:22 -0400

    added one to counter

--- labs/lab4/js/app.js
+++ labs/lab4/js/app.js
@@ -5,8 +5,9 @@ function onPlayClick(event) {
     if(counter > 9) {
         return;
     } else if(counter % 2 == 0) {
-        event.target.xlink:href = oPlayer.png
+        event.target.xlink = "oPlayer.png"
     } else {
-        event.target.xlink:href = oPlayer.png
+        event.target.xlink = "xPlayer.png"
     }
+    counter++;
 }
