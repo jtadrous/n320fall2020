7d0e2e73fb85b2d2d3756ce77a4a9633405c6bcd
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-08 00:35:04 -0400

    pulled each of the rectangles in JS

---
+++ labs/lab5/js/app.js
@@ -0,0 +1,9 @@
+let shapes = [];
+shapes.push(document.getElementById("big"));
+shapes.push(document.getElementById("small1"));
+shapes.push(document.getElementById("small2"));
+shapes.push(document.getElementById("small3"));
+shapes.push(document.getElementById("small4"));
+shapes.push(document.getElementById("small5"));
+shapes.push(document.getElementById("small6"));
+console.log(shapes);
