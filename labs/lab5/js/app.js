16d492c8533b4bd69bca12dc777de83f59aa8af6
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-08 01:03:07 -0400

    created highlight and reColor functions for mouse events

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -1,6 +1,10 @@
 //Created an empty array
 let shapes = [];
 
+//Added an event for whenever a user mouses over an element
+addEventListener("mouseover", highLight);
+addEventListener("mouseout", reColor);
+
 //Pulled each of the rectangles by their ids and pushed them into the shapes array
 shapes.push(document.getElementById("small6"));
 shapes.push(document.getElementById("small5"));
@@ -15,4 +19,18 @@ console.log(shapes);
 for (i = 0; i < shapes.length; i++) {
     //Used GreenSock for the animation and delayed the opacity change for each rectangle
     TweenMax.to(shapes[i], {duration: 0.7*i, opacity: 1});
+}
+
+function highLight(event) {
+    if (event.target.id != "") {
+        TweenMax.to(event.target, {backgroundColor: "#b5ceff"});
+    }
+}
+
+function reColor(event) {
+    if (event.target.id == "big") {
+        TweenMax.to(event.target, {backgroundColor: "#84a4e3"});
+    } else if (event.target.id != "") {
+        TweenMax.to(event.target, {backgroundColor: "#6d873a"});
+    }
 }
