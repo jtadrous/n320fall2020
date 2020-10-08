c1f17e62d38e81db10a611c991b7362b40a25cb5
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-08 01:38:25 -0400

    changing classes and ids

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -7,13 +7,8 @@ addEventListener("mouseout", reColor);
 addEventListener("click", exitGallery);
 
 //Pulled each of the rectangles by their ids and pushed them into the shapes array
-shapes.push(document.getElementById("small6"));
-shapes.push(document.getElementById("small5"));
-shapes.push(document.getElementById("small4"));
-shapes.push(document.getElementById("small3"));
-shapes.push(document.getElementById("small2"));
-shapes.push(document.getElementById("small1"));
-shapes.push(document.getElementById("big"));
+shapes.push(document.getElementsByClassName("small"));
+//shapes.push(document.getElementById("big"));
 console.log(shapes);
 
 //Created a for loop to run through the array of rectangles and animate the opacities to change
@@ -24,7 +19,7 @@ for (i = 0; i < shapes.length; i++) {
 
 //Created a function to change the rectangle color when hovered over
 function highLight(event) {
-    if (event.target.id) {
+    if (event.target.id || event.target.class) {
         //Used GreenSock for the animation and changed the element's color to gray
         TweenMax.to(event.target, {backgroundColor: "#adadad"});
     }
@@ -35,7 +30,7 @@ function reColor(event) {
     if (event.target.id == "big") {
         //Used GreenSock for the animation and changed the element's color to light blue
         TweenMax.to(event.target, {backgroundColor: "#84a4e3"});
-    } else if (event.target.id) {
+    } else if (event.target.class == "small") {
         //Used GreenSock for the animation and changed the element's color to army green
         TweenMax.to(event.target, {backgroundColor: "#6d873a"});
     }
@@ -43,7 +38,7 @@ function reColor(event) {
 
 //Created a function to make the rectangles exit off screen when each one is clicked
 function exitGallery(event) {
-    if (event.target.id) {
+    if (event.target.id || event.target.class) {
         //Used GreenSock for the animation and delayed the x coordinate change for each rectangle
         //this makes each rectangle move left off the screen
         TweenMax.to(event.target, {duration: 1, x: -1000});
