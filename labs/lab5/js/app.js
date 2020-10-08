4a19ef608fe3d4fbbb9aa9739daae20114277cc5
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-08 00:51:32 -0400

    comments

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -1,4 +1,7 @@
+//Created an empty array
 let shapes = [];
+
+//Pulled each of the rectangles by their ids and pushed them into the shapes array
 shapes.push(document.getElementById("small6"));
 shapes.push(document.getElementById("small5"));
 shapes.push(document.getElementById("small4"));
@@ -8,6 +11,8 @@ shapes.push(document.getElementById("small1"));
 shapes.push(document.getElementById("big"));
 console.log(shapes);
 
+//Created a for loop to run through the array of rectangles and animate the opacities to change
 for (i = 0; i < shapes.length; i++) {
+    //Used GreenSock for the animation and delayed the opacity change for each rectangle
     TweenMax.to(shapes[i], {duration: 0.7*i, opacity: 1});
 }
