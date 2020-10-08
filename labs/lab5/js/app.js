aefdee53a6637d3eba5526e1d4c79babaf59ab6b
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-08 01:06:41 -0400

    commenting

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -1,9 +1,10 @@
 //Created an empty array
 let shapes = [];
 
-//Added an event for whenever a user mouses over an element
+//Added an event for whenever a user mouses over, mouses off, and clicks an element
 addEventListener("mouseover", highLight);
 addEventListener("mouseout", reColor);
+addEventListener("click", exitGallery);
 
 //Pulled each of the rectangles by their ids and pushed them into the shapes array
 shapes.push(document.getElementById("small6"));
@@ -21,12 +22,14 @@ for (i = 0; i < shapes.length; i++) {
     TweenMax.to(shapes[i], {duration: 0.7*i, opacity: 1});
 }
 
+//Created a function to change the rectangle color when hovered over
 function highLight(event) {
     if (event.target.id != "") {
-        TweenMax.to(event.target, {backgroundColor: "#b5ceff"});
+        TweenMax.to(event.target, {backgroundColor: "#adadad"});
     }
 }
 
+//Created a function to change the rectangle color back to the original when mouse is off of it
 function reColor(event) {
     if (event.target.id == "big") {
         TweenMax.to(event.target, {backgroundColor: "#84a4e3"});
