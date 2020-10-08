25053fc3e1712c79866a4e8e1e291701b0584847
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-08 00:49:42 -0400

    created a for loop to change the opacitites of each rectangle

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -1,9 +1,13 @@
 let shapes = [];
-shapes.push(document.getElementById("big"));
-shapes.push(document.getElementById("small1"));
-shapes.push(document.getElementById("small2"));
-shapes.push(document.getElementById("small3"));
-shapes.push(document.getElementById("small4"));
-shapes.push(document.getElementById("small5"));
 shapes.push(document.getElementById("small6"));
+shapes.push(document.getElementById("small5"));
+shapes.push(document.getElementById("small4"));
+shapes.push(document.getElementById("small3"));
+shapes.push(document.getElementById("small2"));
+shapes.push(document.getElementById("small1"));
+shapes.push(document.getElementById("big"));
 console.log(shapes);
+
+for (i = 0; i < shapes.length; i++) {
+    TweenMax.to(shapes[i], {duration: 0.7*i, opacity: 1});
+}
