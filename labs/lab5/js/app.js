5a407e0c8b0e86a0f9e00893ac786800316303cc
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-08 02:39:08 -0400

    moved event listeners into for loop

--- labs/lab5/js/app.js
+++ labs/lab5/js/app.js
@@ -1,8 +1,3 @@
-//Added an event for whenever a user mouses over, mouses off, and clicks an element
-addEventListener("mouseover", highLight);
-addEventListener("mouseout", reColor);
-addEventListener("click", exitGallery);
-
 //Pulled each of the rectangles by their className and pushed them into the shapes array
 let shapes = document.getElementsByClassName("rect");
 console.log(shapes);
@@ -11,14 +6,17 @@ console.log(shapes);
 for (i = 0; i < shapes.length; i++) {
     //Used GreenSock for the animation and delayed the opacity change for each rectangle
     TweenMax.to(shapes[i], {duration: 0.7*i, opacity: 1});
+
+    //Added an event for whenever a user mouses over, mouses off, and clicks an element
+    shapes[i].addEventListener("mouseover", highLight);
+    shapes[i].addEventListener("mouseout", reColor);
+    shapes[i].addEventListener("click", exitGallery);
 }
 
 //Created a function to change the rectangle color when hovered over
 function highLight(event) {
-    if (event.target.className == "rect") {
-        //Used GreenSock for the animation and changed the element's color to gray
-        TweenMax.to(event.target, {backgroundColor: "#adadad"});
-    }
+    //Used GreenSock for the animation and changed the element's color to gray
+    TweenMax.to(event.target, {backgroundColor: "#adadad"});
 }
 
 //Created a function to change the rectangle color back to the original when mouse is off of it
@@ -26,7 +24,7 @@ function reColor(event) {
     if (event.target.id == "big") {
         //Used GreenSock for the animation and changed the element's color to light blue
         TweenMax.to(event.target, {backgroundColor: "#84a4e3"});
-    } else if (event.target.className == "rect") {
+    } else {
         //Used GreenSock for the animation and changed the element's color to army green
         TweenMax.to(event.target, {backgroundColor: "#6d873a"});
     }
@@ -34,9 +32,7 @@ function reColor(event) {
 
 //Created a function to make the rectangles exit off screen when each one is clicked
 function exitGallery(event) {
-    if (event.target.className == "rect") {
-        //Used GreenSock for the animation and delayed the x coordinate change for each rectangle
-        //this makes each rectangle move left off the screen
-        TweenMax.to(event.target, {duration: 1, x: -1000});
-    }
+    //Used GreenSock for the animation and delayed the x coordinate change for each rectangle
+    //this makes each rectangle move left off the screen
+    TweenMax.to(event.target, {duration: 1, x: -1000});
 }
