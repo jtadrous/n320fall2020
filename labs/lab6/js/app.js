7318523eefbe4fc821cd56874ec9053f35bcae5b
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-16 00:53:47 -0400

    added an if statement to check the now property of each book

--- labs/lab6/js/app.js
+++ labs/lab6/js/app.js
@@ -1,7 +1,7 @@
 //Setup a new Vue Component
 Vue.component("book-view", {
     props: ["book"],
-    template: "<div>{{book.title}}<br/>{{book.author}}<br/>{{book.emoji}}</div>"
+    template: "<div v-if=\"book.now\">{{book.title}}<br/>{{book.author}}<br/>{{book.emoji}}</div>"
 });
 
 let counter = 0;
@@ -11,15 +11,28 @@ let app = new Vue ({
     el: "#app",
     data: {
         books: [
-            {id: 1, title: "The Book Thief", author: "Markus Zusak", emoji: "e", now: true},
-            {id: 2, title: "The Maze Runner", author: "James Dashner", emoji: "e", now: false},
-            {id: 3, title: "Truly Devious", author: "Maureen Johnson", emoji: "e", now: false}
+            {id: 1, title: "The Book Thief", author: "Markus Zusak", emoji: "emoji", now: true},
+            {id: 2, title: "The Maze Runner", author: "James Dashner", emoji: "emoji", now: false},
+            {id: 3, title: "Truly Devious", author: "Maureen Johnson", emoji: "emoji", now: false}
         ]
     },
     methods: {
         showBook: function () {
             counter++;
-
+            if (counter == 1) {
+                this.books[0].now = true;
+                this.books[1].now = false;
+                this.books[2].now = false;
+            } else if (counter == 2) {
+                this.books[0].now = false;
+                this.books[1].now = true;
+                this.books[2].now = false;
+            } else if (counter == 3) {
+                this.books[0].now = false;
+                this.books[1].now = false;
+                this.books[2].now = true;
+                counter = 0;
+            }
         }
     }
 });
