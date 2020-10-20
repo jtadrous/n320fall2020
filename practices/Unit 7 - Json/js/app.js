286d52da19b644c5e299e51190c0236fc37baa18
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-20 17:43:55 -0400

    created an axios connection and a Vue application

---
+++ practices/Unit 7 - Json/js/app.js
@@ -0,0 +1,14 @@
+axios.get("js/data.json")
+    .then(function (response) {
+        console.log(response);
+    })
+
+let app = new Vue ({
+    el: "#app",
+    data: {
+
+    },
+    methods: {
+        
+    }
+});
