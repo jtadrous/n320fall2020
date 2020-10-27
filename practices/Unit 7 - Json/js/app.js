ca74c4203af6e5887425fc14d3fa5a22d510f65b
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-10-27 02:10:07 -0400

    setup a new vue application and component

--- practices/Unit 7 - Json/js/app.js
+++ practices/Unit 7 - Json/js/app.js
@@ -1,14 +1,35 @@
-axios.get("js/data.json")
+Vue.component("data-view", {
+    props: ["phones"],
+    template: "<div v-for=\"phone in phones\">Hello {{ phone.phone }} - {{ phone.type }}</div>"
+});
+
+//Axios method
+axios.get("data/data.json")
     .then(function (response) {
         console.log(response);
-    })
+    });
 
-let app = new Vue ({
-    el: "#app",
-    data: {
+    let app = new Vue ({
+        el: "#app",
+        data: function () {
+            return {
+              phones: null,
+            };
+        },
+        mounted: function () {
+            fetch("data/data.json")
+              .then((response) => response.json())
+              .then((jsonData) => {
+                this.phones = jsonData;
+              });
+          },
+    });
 
-    },
-    methods: {
-        
-    }
-});
+//Fetch method
+/*fetch ("data/data.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        console.log(data.phone);
+        this.phones = data;
+    }); //creates a js object called data*/
