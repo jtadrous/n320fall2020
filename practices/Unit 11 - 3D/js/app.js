e3c513c8f4236b302ea718c4b96a53cdb70443e8
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-11-17 23:04:44 -0500

    added light, torus, and ground

--- practices/Unit 11 - 3D/js/app.js
+++ practices/Unit 11 - 3D/js/app.js
@@ -1,8 +1,7 @@
 var canvas = document.getElementById("myCanvas");
 
 var engine = new BABYLON.Engine(canvas, true); //true means use WebGL
-var camera;
-var scene;
+var camera, scene;
 
 function createScene() {
     //Create the new scene
@@ -11,6 +10,15 @@ function createScene() {
     //Add a camera to the scene (points to the center)
     camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 4, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
+
+    //Add lights to scene
+    var light = BABYLON.DirectionalLight("Light1", new BABYLON.Vector3(0, -0.5, 1.0), scene);
+
+    //Add in a mesh shape
+    var torus = BABYLON.MeshBuilder.CreateTorus("Torus", {thickness: 0.5, diameter: 3}, scene);
+    
+    //Add and manipulate meshes in the scene
+    var ground = BABYLON.MeshBuilder.CreateGround("Ground", {height: 2, width: 2, subdivisions: 4}, scene);
 
     return scene
 };
