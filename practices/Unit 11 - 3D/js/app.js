f35f0b280a5bd612141ea2afe7745deafc0bc668
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-11-17 21:11:48 -0500

    update scene

--- practices/Unit 11 - 3D/js/app.js
+++ practices/Unit 11 - 3D/js/app.js
@@ -2,14 +2,20 @@ var canvas = document.getElementById("myCanvas");
 
 var engine = new BABYLON.Engine(canvas, true); //true means use WebGL
 var camera;
+var scene;
 
 function createScene() {
     //Create the new scene
     var scene = new BABYLON.Scene(engine);
 
-    //Add a camera to the scene
+    //Add a camera to the scene (points to the center)
     camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 4, 4, BABYLON.Vector3.Zero(), scene);
     camera.attachControl(canvas, true);
 
     return scene
 };
+
+scene = createScene();
+engine.runRenderLoop(function () {
+    scene.render();
+});
