7a3083e2a07dff1b490c9e1e68c612ae0f9ac54c
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-11-18 11:12:56 -0500

    decorated scene with new elements

--- practices/Unit 11 - 3D/js/app.js
+++ practices/Unit 11 - 3D/js/app.js
@@ -1,8 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 
 var engine = new BABYLON.Engine(canvas, true); //true means use WebGL
-var camera;
-var scene;
+var camera, scene, sphere, light;
+var t = 0;
 
 function createScene() {
     //Create the new scene
@@ -13,18 +13,34 @@ function createScene() {
     camera.attachControl(canvas, true);
 
     //Add lights to scene
-    var light = new BABYLON.DirectionalLight("Light1", new BABYLON.Vector3(0, -0.5, 1.0), scene);
+    //var light = new BABYLON.DirectionalLight("Light1", new BABYLON.Vector3(0, -0.5, 1.0), scene);
 
     //Add in a mesh shape
     var torus = BABYLON.MeshBuilder.CreateTorus("Torus", {thickness: 0.2, diameter: 0.5}, scene);
-    
+    torus.position.y = -1;
+
     //Add and manipulate meshes in the scene
     var ground = BABYLON.MeshBuilder.CreateGround("Ground", {height: 2, width: 2, subdivisions: 4}, scene);
+    ground.position.y = -1;
+
+    //Something fun
+    //Add and manipulate meshes in the scene
+    sphere = BABYLON.MeshBuilder.CreateSphere("Sphere1", {diameter: .7}, scene);
+    var lesserSphere = BABYLON.MeshBuilder.CreateSphere("Sphere2", {diameter: .2}, scene);
+    lesserSphere.position.x = 1;
+    sphere.addChild(lesserSphere);
+
+    light = new BABYLON.HemisphericLight("HemiLight", new BABYLON.Vector3(1, 1, 0), scene);
+    light.diffuse = new BABYLON.Color3(0, 0, 1);
+    light.specular = new BABYLON.Color3(0, 0, 0);
+    light.groundColor = new BABYLON.Color3(1, 0, 0);
 
     return scene;
 };
 
 scene = createScene();
 engine.runRenderLoop(function () {
+    t += 0.02;
+    light.intensity = 1 + Math.sin(t);
     scene.render();
 });
