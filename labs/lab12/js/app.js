7212d9f54bc5614167d133f4c3877514263bc806
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-12-04 15:38:45 -0500

    Add a physics impostor to the ground

--- labs/lab12/js/app.js
+++ labs/lab12/js/app.js
@@ -33,10 +33,14 @@ function createScene() {
         {mass: 1, restitution: .2}, scene
     );
 
-    //Creating the ground of the scene
+    //Creating the ground of the scene and physics impostor
     var ground = BABYLON.MeshBuilder.CreateGround("Ground", {height: 20, width: 20, subdivisions: 4}, scene);
     ground.position.y = -3;
     ground.position.z = 10;
+    ground.physicsImpostor = new BABYLON.PhysicsImpostor(
+        ground, BABYLON.PhysicsImpostor.BoxImpostor,
+        {mass: 0, restitution: .9}, scene
+    );
 
     return scene;
 }
