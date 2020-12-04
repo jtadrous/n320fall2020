2f2c57763c20584d03f5b4a9c8032c3b7dfe11d3
Julie Tadrous <35301030+jtadrous@users.noreply.github.com>
2020-12-04 15:30:05 -0500

    Adding physics to the sphere

--- labs/lab12/js/app.js
+++ labs/lab12/js/app.js
@@ -20,8 +20,18 @@ function createScene() {
     //Creating a light
     var light = new BABYLON.DirectionalLight("Light", new BABYLON.Vector3(0,-.5,1), scene);
 
-    //Creating a sphere
+
+    //Enable the physics with cannon.js
+    var gravity = BABYLON.Vector3(0, -9.81, 0);
+    var physics = new BABYLON.CannonJSPlugin();
+    scene.enablePhysics(gravity, physics);
+
+    //Creating a sphere and physics impostor
     ball = BABYLON.MeshBuilder.CreateSphere("Sphere", {diameter: 1}, scene);
+    ball.physicsImpostor = new BABYLON.physicsImpostor(
+        ball, BABYLON.PhysicsImpostor.SphereImpostor,
+        {mass: 1, restitution: .2}, scene
+    );
 
     return scene;
 }
